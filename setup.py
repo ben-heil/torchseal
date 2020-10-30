a9a76e97ca9502a00697de9432483eb4bb83a75b
Ben Heil <ben.jer.heil@gmail.com>
2020-10-30 10:20:57 -0400

    Added setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='torchseal',
+      version='1.0',
+      description='A utility for finding memory leaks in pytorch',
+      author='Ben Heil',
+      author_email='ben.jer.heil@gmail.com',
+      url='https://github.com/ben-heil/torchseal',
+      packages=['torchseal'],
+      install_requires=['pytorch'],
+      tests_require=['pytorch', 'torchvision', 'pytest']
+      )
